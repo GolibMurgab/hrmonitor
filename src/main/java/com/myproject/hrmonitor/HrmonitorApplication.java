d4783194889d638890e4923e308566f9287853a2
Gleb <koanec5000@gmail.com>
2025-03-05 21:11:22 +0300

    initial commit

---
+++ src/main/java/com/myproject/hrmonitor/HrmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.hrmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/hrmonitor/HrmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.hrmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
